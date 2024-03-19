c089fc9febd3f177bc15feefab785a64e323c7b4
MaratKarsanov <marat.karsanov@mail.ru>
2024-03-19 12:41:59 +0300

    Убрал ненужный конструктор у Pager Добавил константу PaginationButtonsCount

--- OnlineShop/OnlineShopWebApp/Repositories.cs
+++ OnlineShop/OnlineShopWebApp/Repositories.cs
@@ -6,11 +6,11 @@ namespace OnlineShopWebApp
     {
         public static Repository<Product> ProductRepository = new Repository<Product>();
 
-        public static Repository<Cart> CartRepository = new Repository<Cart> ();
+        public static Repository<Cart> CartRepository = new Repository<Cart>();
 
         static Repositories()
         {
-            for (var i = 0; i < 100; i++)
+            for (var i = 0; i < 1000; i++)
                 ProductRepository.Add(new Product($"Name{i + 1}", (i + 1) * 100));
         }
     }
