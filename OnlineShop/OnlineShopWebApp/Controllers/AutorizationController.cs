9169ef220e055e666e844d00da61b174e3453e0d
MaratKarsanov <marat.karsanov@mail.ru>
2024-03-26 15:59:33 +0300

    -Добавил AutorizationController -Добавил кнопку авторизации в head -Создал представление для авторизации, принимающее данные от пользователя -Создал класс AutorizationData для хранения информации об авторизации

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AutorizationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AutorizationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn(AutorizationData autorizationData)
+        {
+            return View("Success", autorizationData);
+        }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Models/AutorizationData.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class AutorizationData
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public override string ToString()
+        {
+            return $"Логин: {Login} Пароль: {Password}";
+        }
+    }
+}
