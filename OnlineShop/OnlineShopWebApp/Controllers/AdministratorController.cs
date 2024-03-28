986eb58a4af056858d1426f8bf527545821bd654
MaratKarsanov <marat.karsanov@mail.ru>
2024-03-28 13:51:48 +0300

    Добавил панель администратора -Создал AdministratorController -Добавил представление для меню администратора -Добавил отображения для пунктов меню администратора

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AdministratorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AdministratorController : Controller
+    {
+        public IActionResult ViewOrders()
+        {
+            return View("Orders");
+        }
+
+        public IActionResult ViewUsers()
+        {
+            return View("Users");
+        }
+
+        public IActionResult ViewRoles()
+        {
+            return View("Roles");
+        }
+
+        public IActionResult ViewProducts()
+        {
+            return View("Products");
+        }
+    }
+}
