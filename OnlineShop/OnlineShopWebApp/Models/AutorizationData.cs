2f21b9bebc7bbfe4290cbf683afc5b2b049cf677
MaratKarsanov <marat.karsanov@mail.ru>
2024-03-28 12:07:50 +0300

    Добавил авторизация: -Создал AutorizationController -Добавил предствление для авторизиции -Cделал класс для хранения данных об авторизации

---
+++ OnlineShop/OnlineShopWebApp/Models/AutorizationData.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class AutorizationData
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
+        public override string ToString()
+        {
+            return $"Логин: {Login} Пароль: {Password} Запомнить? {RememberMe}";
+        }
+    }
+}
