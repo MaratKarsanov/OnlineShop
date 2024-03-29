1ba35ad8eb4ee74bb9f6680f09b3e7a7f19d4c3f
MaratKarsanov <marat.karsanov@mail.ru>
2024-03-29 14:30:12 +0300

    Убрал из AutorizationData флаг "Запомнить меня"

--- OnlineShop/OnlineShopWebApp/Models/AutorizationData.cs
+++ OnlineShop/OnlineShopWebApp/Models/AutorizationData.cs
@@ -4,11 +4,10 @@
     {
         public string Login { get; set; }
         public string Password { get; set; }
-        public bool RememberMe { get; set; }
 
         public override string ToString()
         {
-            return $"Логин: {Login} Пароль: {Password} Запомнить? {RememberMe}";
+            return $"Логин: {Login} Пароль: {Password}";
         }
     }
 }
