75a727a371e7283bb212ee5efa0bca086997aa12
MaratKarsanov <marat.karsanov@mail.ru>
2024-04-05 15:01:45 +0300

    Сделал отображение статуса заказа по русски

---
+++ OnlineShop/OnlineShopWebApp/Helpers/EnumHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace OnlineShopWebApp.Helpers
+{
+    public static class EnumHelper
+    {
+        public static string GetDisplayName(Enum enumValue)
+        {
+            return enumValue
+                .GetType()
+                .GetMember(enumValue.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>()
+                .GetName();
+        }
+    }
+}
--- OnlineShop/OnlineShopWebApp/Models/OrderStatus.cs
+++ OnlineShop/OnlineShopWebApp/Models/OrderStatus.cs
@@ -4,10 +4,19 @@ namespace OnlineShopWebApp.Models
 {
     public enum OrderStatus
     {
+        [Display(Name = "Создан")]
         Created,
+
+        [Display(Name = "Обработан")]
         Processed,
+
+        [Display(Name = "В пути")]
         Delivering,
+
+        [Display(Name = "Отменен")]
         Cancelled,
+
+        [Display(Name = "Доставлен")]
         Delivered
     }
 }
